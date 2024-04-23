e8a338949958a497e53fc78db4c6f8f0886c6c32
SurajPanigrahi <surajpanigrahi70711@gmail.com>
2024-04-23 21:18:21 +0530

    Lecture 3

---
+++ Lect3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+// This Program was Created by Suraj and this is a single line comment
+/* This Comment is also created by Suraj
+and This is a Multi line comment*/
+
+// Comments are non executable code that are not compiled/executed
+
+int main()
+{
+    int sum=6;
+    cout<<"Hello World! "<<sum;
+    return 0;
+}
